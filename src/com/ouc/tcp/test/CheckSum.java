fb16ac4b0158434d85e9e9d080fcf1751122d678
Kappa9 <893387721@qq.com>
2020-12-28 15:21:01 +0800

    初版

---
+++ src/com/ouc/tcp/test/CheckSum.java
@@ -0,0 +1,21 @@
+package com.ouc.tcp.test;
+
+import java.util.Arrays;
+import java.util.zip.CRC32;
+
+import com.ouc.tcp.message.TCP_HEADER;
+import com.ouc.tcp.message.TCP_PACKET;
+
+public class CheckSum {
+    /*计算TCP报文段校验和：只需校验TCP首部中的seq、ack和sum，以及TCP数据字段*/
+    public static short computeChkSum(TCP_PACKET tcpPack) {
+        short checkSum = 0;
+        CRC32 crc = new CRC32();
+        crc.update(tcpPack.getTcpH().getTh_seq());
+        crc.update(tcpPack.getTcpH().getTh_ack());
+        crc.update(Arrays.toString(tcpPack.getTcpS().getData()).getBytes());
+        //计算校验和
+        checkSum = (short) crc.getValue();
+        return checkSum;
+    }
+}
